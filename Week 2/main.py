c81ef70d58f2f7ab5bda2efaf5df0f2f8849b4fb
BTank01 <brandonbirch10@outlook.com>
2022-02-14 11:21:14 +0000

    Added menu for options

--- Week 2/main.py
+++ Week 2/main.py
@@ -1,11 +1,28 @@
 # Week 2 python tasks.
 from ClassifyData import *
 from ClassifyDataWithPandas import *
+from classifyDigitImages import *
 
 if __name__ == '__main__':
-    # Uses KNN to classify flowers
-    #ClassifyData()
-    # Process data using pandas
-    PandasClassify()
+    message = """
+    |=======================================|
+    |   Select an Option:                   |
+    |   1: Iris Dataset using Sklearn       |
+    |   2: Iris Dataset using Pandas        |
+    |   3: Handwritten Digits Dataset       |
+    |=======================================|
+    """
 
-
+    while True:
+        clearConsole = lambda: print('\n' * 150)
+        clearConsole()
+        useSelect = int(input(message))
+        if useSelect == 1:
+            # Uses KNN to classify flowers
+            ClassifyData()
+        elif useSelect == 2:
+            # Process data using pandas
+            PandasClassify()
+        elif useSelect == 3:
+            # Uses KNN to classify handwritten numbers
+            classifyDigitImages()
