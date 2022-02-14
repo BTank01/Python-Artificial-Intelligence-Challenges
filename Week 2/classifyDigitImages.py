0f1ba7299dca7ff633c215273e82df8dc7b394bb
BTank01 <brandonbirch10@outlook.com>
2022-02-14 11:44:30 +0000

    Classifies Digits

--- Week 2/classifyDigitImages.py
+++ Week 2/classifyDigitImages.py
@@ -24,4 +24,24 @@ def classifyDigitImages():
     plt.show()
     """
 
-    # CLassify Data
+    # Classify Data
+    # Change dataset into two dimensions using PCA
+    pca = PCA(n_components=2)
+    compressed = pca.fit_transform(data)
+
+    # Trains the classifier using 10 closest neighbors
+    classifier = KNeighborsClassifier(n_neighbors=10)
+    # Fit input to targets
+    classifier.fit(data, target)
+    # Tries to predict for all images
+    classifiedData = classifier.predict(data)
+
+    # plots the results in a 2D scatter graph
+    plt.figure()
+    plt.scatter(compressed[:, 0], compressed[:, 1], c=classifiedData)
+    plt.title("PCA Projection")
+    plt.savefig("digits_pca_pre-accuracy_classified.png", bbox_inches="tight")
+    plt.show()
+
+
+    #
