c4dc5bedfc1d692b17c0c648ede4a2f67fdbb975
BTank01 <brandonbirch10@outlook.com>
2022-02-16 10:31:57 +0000

    Data normalised

--- Week 3/HouseDataRegression.py
+++ Week 3/HouseDataRegression.py
@@ -1,6 +1,7 @@
 from sklearn.datasets import fetch_california_housing
 from sklearn.neural_network import MLPRegressor
 from sklearn.metrics import mean_absolute_error
+from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
 
 def houseRegression():
@@ -9,10 +10,14 @@ def houseRegression():
     inputs = data["data"]
     targets = data["target"]
 
-    # Show sata on graph
+    # Show data on graph
     """plt.figure()
     plt.scatter(inputs[:, 0], inputs[:, 1], c=targets, cmap="viridis")
     plt.colorbar()
     plt.show()"""
 
+    # Normalise Data
+    scaler = MinMaxScaler()
+    modelData = scaler.fit(inputs)
+    scaledData = modelData.transform(inputs)
 
