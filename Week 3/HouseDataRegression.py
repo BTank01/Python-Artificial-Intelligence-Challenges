79b1da1be82f898116462a938df9917e311f784c
BTank01 <brandonbirch10@outlook.com>
2022-02-16 10:20:33 +0000

    Loads the housing data

--- Week 3/HouseDataRegression.py
+++ Week 3/HouseDataRegression.py
@@ -1,2 +1,18 @@
+from sklearn.datasets import fetch_california_housing
+from sklearn.neural_network import MLPRegressor
+from sklearn.metrics import mean_absolute_error
+import matplotlib.pyplot as plt
+
 def houseRegression():
-    pass
+    # Loads the data
+    data = fetch_california_housing()
+    inputs = data["data"]
+    targets = data["target"]
+
+    # Show sata on graph
+    """plt.figure()
+    plt.scatter(inputs[:, 0], inputs[:, 1], c=targets, cmap="viridis")
+    plt.colorbar()
+    plt.show()"""
+
+
