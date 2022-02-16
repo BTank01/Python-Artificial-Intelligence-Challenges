5c050aeb81d2e75fa2a84cfe95d1a4694c9c8f2b
BTank01 <brandonbirch10@outlook.com>
2022-02-16 10:01:56 +0000

    Classifies iris data

--- Week 3/ClassifyIrisData.py
+++ Week 3/ClassifyIrisData.py
@@ -1,2 +1,19 @@
+from sklearn.datasets import load_iris
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import mean_absolute_error
+
 def classifyIrisData():
-    pass
+    # Load Dataset
+    data = load_iris()
+    inputs = data.data
+    targets = data.target
+
+    # Create Neural Network for classifying
+    classifier = MLPClassifier()
+    # Train Model
+    classifier.fit(inputs, targets)
+    # Make predictions
+    outputs = classifier.predict(inputs)
+    # Find the error rate
+    errorVal = mean_absolute_error(targets, outputs)
+    print(f"Error Rate: {errorVal}")
